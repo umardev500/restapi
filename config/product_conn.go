30dda556bf103831cdd8c1be55dc18df3b886e4a
umardev500 <umar.dev500@gmail.com>
2023-09-26 05:32:11 +0700

    add product rpc connection

---
+++ config/product_conn.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewProductRPC() (conn *grpc.ClientConn) {
+	port := os.Getenv("PRODUCT_PORT")
+	conn, err := grpc.Dial(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+	defer conn.Close()
+
+	return
+}
