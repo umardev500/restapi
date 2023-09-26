bf5348d436f08a3e13b6f6160b4f2a7793f64b0c
umardev500 <umar.dev500@gmail.com>
2023-09-26 23:50:37 +0700

    combine product domain to single file

--- domain/product_domain.go
+++ domain/product_domain.go
@@ -1,7 +1,20 @@
 package domain
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/umardev500/store/proto"
+)
 
 type ProductHanlder interface {
 	Create(c *fiber.Ctx) error
+}
+
+type ProductService interface {
+	CreateProduct(ctx context.Context, p *proto.CreateProductRequest) error
+}
+
+type ProductRepository interface {
+	Create(ctx context.Context, p *proto.CreateProductRequest) error
 }
