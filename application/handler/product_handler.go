f5c720a5ec8c1e9ce8f23b2c11ecf61e73cc93a0
umardev500 <umar.dev500@gmail.com>
2023-09-28 19:42:25 +0700

    add response helper

--- application/handler/product_handler.go
+++ application/handler/product_handler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/umardev500/restapi/domain"
 	"github.com/umardev500/restapi/domain/model"
+	"github.com/umardev500/restapi/helper"
 )
 
 type productHandler struct {
@@ -25,20 +26,25 @@ func NewProductHandler(service domain.ProductService, validator *validator.Valid
 func (ph *productHandler) Create(c *fiber.Ctx) error {
 	var product *model.ProductStore = new(model.ProductStore)
 	if err := c.BodyParser(product); err != nil {
-		return c.SendStatus(fiber.StatusBadRequest)
+		return helper.ErrResponse(c, fiber.StatusUnprocessableEntity, nil, err.Error())
 	}
 
 	// validate the struct
 	if err := ph.validator.Struct(product); err != nil {
-		return c.SendStatus(fiber.StatusUnprocessableEntity)
+		var details []string
+		for _, fieldErr := range err.(validator.ValidationErrors) {
+			details = append(details, fieldErr.Error())
+		}
+
+		return helper.ErrResponse(c, fiber.StatusUnprocessableEntity, details)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	err := ph.service.CreateProduct(ctx, *product)
 	if err != nil {
-		return c.JSON(err.Error())
+		return helper.ErrResponse(c, fiber.StatusInternalServerError, nil, err.Error())
 	}
 
-	return c.JSON("ok")
+	return helper.OkResponse(c, fiber.StatusOK, "Product created successfully", nil)
 }
