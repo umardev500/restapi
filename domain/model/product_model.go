da787fe1908af734b7d182c55e399fc2a43ff1e1
umardev500 <umar.dev500@gmail.com>
2023-09-28 19:15:00 +0700

    update validation of struct

--- domain/model/product_model.go
+++ domain/model/product_model.go
@@ -1,8 +1,8 @@
 package model
 
 type ProductStore struct {
-	Name        string   `json:"name" validate:"required"`
-	Description string   `json:"description" validate:"required"`
+	Name        string   `json:"name" validate:"required,min=15"`
+	Description string   `json:"description" validate:"required,min=50"`
 	Price       float32  `json:"price" validate:"required"`
 	Stock       int64    `json:"stock" validate:"required"`
 	Images      []string `json:"images" validate:"required"`
